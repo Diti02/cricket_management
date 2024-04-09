16f9422c3e628bec8664886caa900d50593f04e4
Diti02 <ditibanerjee02@gmail.com>
2024-04-10 01:11:38 +0530

    match optimised

--- myPackage/Match.java
+++ myPackage/Match.java
@@ -7,22 +7,18 @@ public class Match {
     private Team teamA;
     private Team teamB;
     private int oversTotal;
-    private int TeamARuns;
-    private int TeamBRuns;
-    private int TeamAWickets;
-    private int TeamBWickets;
-    private int winner;
+    private int winner;// -1 for tie, 0 for teamA, 1 for teamB
 
     public Match(Team teamA, Team teamB, int oversTotal) {
         this.matchID=matchIDCounter++;
         this.teamA = teamA;
         this.teamB = teamB;
         this.oversTotal = oversTotal;
-        this.TeamARuns=0;
-        this.TeamBRuns=0;
-        this.TeamAWickets=0;
-        this.TeamBWickets=0;
         this.winner=-1;
+    }
+
+    public int getWinner(){
+        return winner;
     }
 
     public int toss(){
@@ -46,80 +42,78 @@ public class Match {
             battingTeam = teamB;
             bowlingTeam = teamA;
         }
-
+        
         System.out.println("Toss result: " + (tossResult == 0 ? teamA.getName() : teamB.getName()) + " won the toss and chose to bat.");
         
-        System.out.println("Batting Team:" + teamA.getName() + "\n" + "Bowling Team:" + teamB.getName());
-        for (int i = 0; i < oversTotal; i++) {
-            System.out.println("Over " + (i + 1) + ":");
-            simulateOver(teamA, teamB);
-            if (teamA.wickets==10) {
+        System.out.println("Batting Team:" + battingTeam.getName() + "\n" + "Bowling Team:" + bowlingTeam.getName());
+        //All overs for 1st batting team
+        for (int i = 1; i <= oversTotal; i++) {
+            System.out.println("Over " + i + ":");
+            simulateOver(battingTeam, bowlingTeam);
+            if (battingTeam.getWicketsFell() == 10) {
                 break;
-            }
-           
+            }           
         }
-        System.out.println("Runs scored by Team A: "+teamA.totalRuns+ ", Wickets: "+ teamA.wickets);
+        System.out.println("Runs scored by : "+battingTeam.getName()+":"+ battingTeam.getTotalRuns()+ ", Wickets: "+ battingTeam.getWicketsFell());
         
-        System.out.println("\n \nBatting Team:" + teamB.getName() + "\n" + "Bowling Team:" + teamA.getName());
-         int f=1;
-         for (int i = 0; i < oversTotal; i++) {
-           System.out.println("Team B requires: "+(teamA.totalRuns-teamB.totalRuns)+ "runs to win");
-            System.out.println("Over " + (i + 1) + ":");
-            simulateOver(teamB, teamA);
-            if (teamB.wickets>teamA.wickets) {
+        System.out.println("\n Batting Team:" + bowlingTeam.getName() + "\n" + "Bowling Team:" + battingTeam.getName());
+        int matchEnd=0;
+        //All overs for 2nd batting team
+         for (int i = 1; i <= oversTotal; i++) {
+           System.out.println(bowlingTeam.getName()+" requires: "+(battingTeam.getTotalRuns()-bowlingTeam.getTotalRuns())+ "runs to win");
+            System.out.println("Over " + i + ":");
+            simulateOver(bowlingTeam, battingTeam);
+
+            //Won by wickets or Won by runs
+            if ( bowlingTeam.getWicketsFell() == 10 || bowlingTeam.getTotalRuns() > battingTeam.getTotalRuns()) {
                 System.out.println("Match finished!");
-                System.out.println(teamA.getName() + " total runs: " + teamA.getTotalRuns());
-                System.out.println(teamB.getName() + " total runs: " + teamB.getTotalRuns());
-                System.out.println("\nTeam A  wins the match ! \n");
-                 f=0;
-                 break;
-            }
-            if(teamB.totalRuns>teamA.totalRuns){
-                System.out.println("Match finished!");
-                System.out.println(teamA.getName() + " total runs: " + teamA.getTotalRuns());
-                System.out.println(teamB.getName() + " total runs: " + teamB.getTotalRuns());
-                System.out.println("\nTeam B wins the match ! \n");
-                 f=0;
-                 break;
-            }
-        }
-        
-        if(f==1){
+                matchEnd=1;
+                System.out.println(battingTeam.getName() + " total runs: " + teamA.getTotalRuns());
+                System.out.println(bowlingTeam.getName() + " total runs: " + bowlingTeam.getTotalRuns());
+                if(bowlingTeam.getWicketsFell() == 10 ){
+                    winner=0;
+                    System.out.println(battingTeam.getName()+" wins the match ! \n");
+                }
+                else if(bowlingTeam.getTotalRuns() > battingTeam.getTotalRuns()){
+                    winner=1;
+                    System.out.println(bowlingTeam.getName()+" wins the match ! \n");
+                }
+                break;
+            }                      
+        }  
+        if(matchEnd==0){  //matchEnd is used to check whether previously match has been ended and result has been displayed or not  
         printMatchResult();
         }
+        
     }
 
    private void simulateOver(Team battingTeam, Team bowlingTeam) {
         Random random = new Random();
-        int sum=0;
-        
-        for (int ball = 0; ball < 6; ball++) {
+        int Totalrun=0;        
+        for (int ball = 1; ball <= 6; ball++) {
             int runs = random.nextInt(8); //0-6 for runs, 7 for wicket
 
             if (runs == 0) {
-                System.out.println("Ball " + (ball + 1) + ": " + battingTeam.getName() + " scores no run.");
+                System.out.println("Ball " + ball + ": " + battingTeam.getName() + " scores no run.");
                 
             } 
             else if (runs == 7) {//random number 7 for wicket
-                battingTeam.wickets++;
-                System.out.println("Ball " + (ball + 1) + ": " + battingTeam.getName() + " 1 wicket fell");
+                battingTeam.updateWicketsFell(1);
+                System.out.println("Ball " + ball + ": " + battingTeam.getName() + " 1 wicket fell");
             } 
             else {
-                System.out.println("Ball " + (ball + 1) + ": " + battingTeam.getName() + " scores " + runs + " runs.");
-                sum+=runs;
+                System.out.println("Ball " + ball + ": " + battingTeam.getName() + " scores " + runs + " runs.");
+                Totalrun+=runs;
                 battingTeam.updateScore(runs);
-            }
-            
-            
-
-            if (battingTeam.wickets==10) {
+            }          
+            if (battingTeam.getWicketsFell()==10) {
                 break;
             }
         }
 
         System.out.println("End of Over: " + battingTeam.getName());
-        System.out.println("Run scored: "+sum+"\n");
-        System.out.println("Wickets "+ battingTeam.wickets +"\n");
+        System.out.println("Runs scored: "+Totalrun);
+        System.out.println("Wickets "+ battingTeam.getWicketsFell() +"\n");
     }
 
     private void printMatchResult() {
@@ -128,10 +122,13 @@ public class Match {
         System.out.println(teamB.getName() + " total runs: " + teamB.getTotalRuns());
 
         if (teamA.getTotalRuns() > teamB.getTotalRuns()) {
-            System.out.println(teamA.getName() + " wins the match by "+ (teamA.totalRuns-teamB.totalRuns) +"runs!");
+            winner=0;
+            System.out.println(teamA.getName() + " wins the match by "+ (teamA.getTotalRuns()-teamB.getTotalRuns()) +"runs!");
         } else if (teamA.getTotalRuns() < teamB.getTotalRuns()) {
-            System.out.println(teamB.getName() + " wins the match by "+ (teamB.totalRuns-teamA.totalRuns)+"runs!");
+            winner=1;
+            System.out.println(teamB.getName() + " wins the match by "+ (teamB.getTotalRuns()-teamA.getTotalRuns())+"runs!");
         } else {
+            winner=-1;
             System.out.println("The match ended in a tie!");
         }
     }
